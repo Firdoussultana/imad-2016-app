074d1d053a69863aee05045f6820774b460c56ea
Firdoussultana <firdouss94@gmail.com>
2016-10-02 11:28:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.get('/aboutme', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'aboutme.html'));
 });
 
+app.get('/edu', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'edu.html'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
