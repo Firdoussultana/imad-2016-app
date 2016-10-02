f418c65413f47dc4f4ccb6a7a85244bb0e2ab595
Firdoussultana <firdouss94@gmail.com>
2016-10-02 19:19:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,14 +90,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/articleName', function (req, res) {
+app.get('/:articleName', function (req, res) {
     var articleName=req.params.articleName;
   res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/edu', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'edu.html'));
-});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
